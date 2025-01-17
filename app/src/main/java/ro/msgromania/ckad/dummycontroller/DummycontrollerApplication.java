c3abceb7b1de22ca4b130c6d2c229efebdf33474
jozsefkind <jozsef.kind@msg.group>
2025-01-17 09:22:05 +0100

    Add a simple web app

---
+++ app/src/main/java/ro/msgromania/ckad/dummycontroller/DummycontrollerApplication.java
@@ -0,0 +1,13 @@
+package ro.msgromania.ckad.dummycontroller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DummycontrollerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DummycontrollerApplication.class, args);
+	}
+
+}
---
+++ app/src/test/java/ro/msgromania/ckad/dummycontroller/DummycontrollerApplicationTests.java
@@ -0,0 +1,13 @@
+package ro.msgromania.ckad.dummycontroller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DummycontrollerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
